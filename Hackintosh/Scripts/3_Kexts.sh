7581c33cd0e7c976b04f739da5fe8f357334d073
Marc <teste74@hotmail.fr>
2024-08-10 00:53:06 +0200

    Update 3_Kexts.sh

--- Hackintosh/Scripts/3_Kexts.sh
+++ Hackintosh/Scripts/3_Kexts.sh
@@ -1,1 +1,33 @@
+#!/usr/bin/bash
+
+########################################################################################################################################
+# Nettoyage de la console #
+###########################
+clear;
+#
+########################################################################################################################################
+# Dossier de Travail #
+######################
+WORK="$HOME/Bureau/Hackintosh"
+
+
+########################################################################################################################################
+# Copie de fichier #
+####################
+#
+# ACPI
+cp $WORK/Ressources/ACPI/SSDT-EC-USBX-DESKTOP.aml $WORK/EFI/OC/ACPI/SSDT-EC-USBX-DESKTOP.aml
+#
+#
+# Kexts
+
+
+cp $WORK/Ressources/Kexts/AppleALC.kext                  $WORK/EFI/OC/Kexts/
+cp $WORK/Ressources/Kexts/AppleMCEReporterDisabler.kext  $WORK/EFI/OC/Kexts/
+cp $WORK/Ressources/Kexts/IntelMausi.kext                $WORK/EFI/OC/Kexts/
+cp $WORK/Ressources/Kexts/Lilu.kext                      $WORK/EFI/OC/Kexts/
+cp $WORK/Ressources/Kexts/VirtualSMC.kext                $WORK/EFI/OC/Kexts/
+cp $WORK/Ressources/Kexts/WhateverGreen.kext             $WORK/EFI/OC/Kexts/
+
+
 
