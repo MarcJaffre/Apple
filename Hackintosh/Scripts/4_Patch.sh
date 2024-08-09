2c45a240f872a2c2f0aacf79fe3fb8aab4a2da4a
Marc <teste74@hotmail.fr>
2024-08-09 23:52:13 +0200

    Create 4_Patch.sh

---
+++ Hackintosh/Scripts/4_Patch.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+########################################################################################################################################
+# Script qui définit sur 8 Cores le patch AMD #
+###############################################
+
+########################################################################################################################################
+# Analyse #
+###########
+# cat $WORK/Ressources/Patch/patches.plist | grep cpuid_cores_per_package -A25 | grep "Replace" -A1
+# 0: <data>uAgAAAAA</data>
+# 1: <data>ugAAAAAA</data>
+# 2: <data>ugAAAACQ</data>
+# 3: <data>ugAAAAA</data>
+
+
+########################################################################################################################################
+# Dossier de Travail #
+######################
+WORK="$HOME/Bureau/Hackintosh"
+
+########################################################################################################################################
+# Nettoyage de la console #
+###########################
+clear;
+
+echo "Configuration du Patch AMD_Vanilla sur 8 Core";
+sed -i -e "s/uAAAAAAA/uAgAAAAA/g" $WORK/Ressources/Patch/patches.plist;
+sed -i -e "s/ugAAAAAA/uggAAAAA/g" $WORK/Ressources/Patch/patches.plist;
+sed -i -e "s/ugAAAACQ/uggAAACQ/g" $WORK/Ressources/Patch/patches.plist;
+sed -i -e "s/ugAAAAA/uggAAAA/g"   $WORK/Ressources/Patch/patches.plist;
+
