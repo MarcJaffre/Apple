7ba8c1cfb69a0b3673c8c5421cfed4840066cba0
Marc <teste74@hotmail.fr>
2024-08-09 23:38:27 +0200

    Update 3_MacRecovery.sh

--- Hackintosh/Scripts/3_MacRecovery.sh
+++ Hackintosh/Scripts/3_MacRecovery.sh
@@ -19,12 +19,10 @@ MONTEREY="-b Mac-FFE5EF870D7BA81A -m 00000000000000000 download"
 VENTURA="-b Mac-4B682C642B45593E -m 00000000000000000 download"
 SONOMA="-b Mac-937A206F2EE63C01 -m 00000000000000000 download"
 
-
 ########################################################################################################################################
 # Nettoyage de la console #
 ###########################
 clear;
-
 
 ########################################################################################################################################
 # Packages #
@@ -35,15 +33,15 @@ if [ ! -f /usr/bin/python3 ];then
 fi
 
 ########################################################################################################################################
-# Telecharger Mac Recovery #
-############################
-
-# Purge Ancîen OS
+# Purge OS #
+############
 if [   -d $WORK/com.apple.recovery.boot ];then
    rm -rf $WORK/com.apple.recovery.boot 2>/dev/null;
 fi
 
-# Telecharger OS
+########################################################################################################################################
+# Telechargement OS #
+#####################
 if [ ! -z $OS ];then
    if [ $OS = "bigsur"   ];then python3 $PROG $BIGSUR   -o $WORK/com.apple.recovery.boot;  fi
    if [ $OS = "monterey" ];then python3 $PROG $MONTEREY -o $WORK/com.apple.recovery.boot;  fi
