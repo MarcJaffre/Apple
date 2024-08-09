be6d26d22dba7af4cd595a509c7e0caea6d3b6f5
Marc <teste74@hotmail.fr>
2024-08-09 23:55:07 +0200

    Update 4_Patch_AMD_Vanilla.sh

--- Hackintosh/Scripts/4_Patch_AMD_Vanilla.sh
+++ Hackintosh/Scripts/4_Patch_AMD_Vanilla.sh
@@ -30,7 +30,7 @@ sed -i -e "s/ugAAAAA/uggAAAA/g"   $WORK/Ressources/Patch/patches.plist;
 ########################################################################################################################################
 # Copie de fichier #
 ####################
-cat $WORK/Ressources/Patch/patches.plist > $WORK/EFI/OC 2>/dev/null
+cat $WORK/Ressources/Patch/patches.plist > $WORK/EFI/OC/patches.plist 2>/dev/null
 
 ########################################################################################################################################
 # Analyse #
