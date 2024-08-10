7d71154ead9677d6fa8bd0fb953c75daf5f65f76
Marc <teste74@hotmail.fr>
2024-08-10 03:33:45 +0200

    Update 3_Kexts.sh

--- Hackintosh/Scripts/3_Kexts.sh
+++ Hackintosh/Scripts/3_Kexts.sh
@@ -20,11 +20,19 @@ cp $WORK/Ressources/ACPI/SSDT-EC-USBX-DESKTOP.aml $WORK/EFI/OC/ACPI/SSDT-EC-USBX
 #
 #
 # Kexts
-cp -r $WORK/Ressources/Kexts/AMDRyzenCPUPowerManagement.kext  $WORK/EFI/OC/Kexts/;
-cp -r $WORK/Ressources/Kexts/AppleALC.kext                    $WORK/EFI/OC/Kexts/;
-cp -r $WORK/Ressources/Kexts/AppleMCEReporterDisabler.kext    $WORK/EFI/OC/Kexts/;
-cp -r $WORK/Ressources/Kexts/IntelMausi.kext                  $WORK/EFI/OC/Kexts/;
-cp -r $WORK/Ressources/Kexts/Lilu.kext                        $WORK/EFI/OC/Kexts/;
-cp -r $WORK/Ressources/Kexts/SMCAMDProcessor.kext             $WORK/EFI/OC/Kexts/;
-cp -r $WORK/Ressources/Kexts/VirtualSMC.kext                  $WORK/EFI/OC/Kexts/;
-cp -r $WORK/Ressources/Kexts/WhateverGreen.kext               $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/AMDRyzenCPUPowerManagement.kext  $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/AppleALC.kext                    $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/AppleMCEReporterDisabler.kext    $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/IntelMausi.kext                  $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/Lilu.kext                        $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/NVMeFix.kext                     $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/RestrictEvents.kext              $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/SMCAMDProcessor.kext             $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/USBInjectAll.kext                $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/VirtualSMC.kext                  $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/WhateverGreen.kext               $WORK/EFI/OC/Kexts/;
+
+########################################################################################################################################
+# Purge #
+#########
+rm -rf $WORK/Ressources/Kexts;
