eb01cb3f9f6675e8ddd9d90a382668e063e8cfed
Marc <teste74@hotmail.fr>
2024-08-10 03:34:42 +0200

    Update 3_Kexts.sh

--- Hackintosh/Scripts/3_Kexts.sh
+++ Hackintosh/Scripts/3_Kexts.sh
@@ -16,21 +16,21 @@ WORK="$HOME/Bureau/Hackintosh"
 ####################
 #
 # ACPI
-mv $WORK/Ressources/ACPI/SSDT-EC-USBX-DESKTOP.aml $WORK/EFI/OC/ACPI/SSDT-EC-USBX-DESKTOP.aml
+mv $WORK/Ressources/ACPI/SSDT-EC-USBX-DESKTOP.aml $WORK/EFI/OC/ACPI/SSDT-EC-USBX-DESKTOP.aml 2>/dev/null;
 #
 #
 # Kexts
-mv $WORK/Ressources/Kexts/AMDRyzenCPUPowerManagement.kext  $WORK/EFI/OC/Kexts/;
-mv $WORK/Ressources/Kexts/AppleALC.kext                    $WORK/EFI/OC/Kexts/;
-mv $WORK/Ressources/Kexts/AppleMCEReporterDisabler.kext    $WORK/EFI/OC/Kexts/;
-mv $WORK/Ressources/Kexts/IntelMausi.kext                  $WORK/EFI/OC/Kexts/;
-mv $WORK/Ressources/Kexts/Lilu.kext                        $WORK/EFI/OC/Kexts/;
-mv $WORK/Ressources/Kexts/NVMeFix.kext                     $WORK/EFI/OC/Kexts/;
-mv $WORK/Ressources/Kexts/RestrictEvents.kext              $WORK/EFI/OC/Kexts/;
-mv $WORK/Ressources/Kexts/SMCAMDProcessor.kext             $WORK/EFI/OC/Kexts/;
-mv $WORK/Ressources/Kexts/USBInjectAll.kext                $WORK/EFI/OC/Kexts/;
-mv $WORK/Ressources/Kexts/VirtualSMC.kext                  $WORK/EFI/OC/Kexts/;
-mv $WORK/Ressources/Kexts/WhateverGreen.kext               $WORK/EFI/OC/Kexts/;
+mv $WORK/Ressources/Kexts/AMDRyzenCPUPowerManagement.kext  $WORK/EFI/OC/Kexts/ 2>/dev/null;
+mv $WORK/Ressources/Kexts/AppleALC.kext                    $WORK/EFI/OC/Kexts/ 2>/dev/null;
+mv $WORK/Ressources/Kexts/AppleMCEReporterDisabler.kext    $WORK/EFI/OC/Kexts/ 2>/dev/null;
+mv $WORK/Ressources/Kexts/IntelMausi.kext                  $WORK/EFI/OC/Kexts/ 2>/dev/null;
+mv $WORK/Ressources/Kexts/Lilu.kext                        $WORK/EFI/OC/Kexts/ 2>/dev/null;
+mv $WORK/Ressources/Kexts/NVMeFix.kext                     $WORK/EFI/OC/Kexts/ 2>/dev/null;
+mv $WORK/Ressources/Kexts/RestrictEvents.kext              $WORK/EFI/OC/Kexts/ 2>/dev/null;
+mv $WORK/Ressources/Kexts/SMCAMDProcessor.kext             $WORK/EFI/OC/Kexts/ 2>/dev/null;
+mv $WORK/Ressources/Kexts/USBInjectAll.kext                $WORK/EFI/OC/Kexts/ 2>/dev/null;
+mv $WORK/Ressources/Kexts/VirtualSMC.kext                  $WORK/EFI/OC/Kexts/ 2>/dev/null;
+mv $WORK/Ressources/Kexts/WhateverGreen.kext               $WORK/EFI/OC/Kexts/ 2>/dev/null;
 
 ########################################################################################################################################
 # Purge #
