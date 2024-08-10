5ae56a3f21d1ab4b2546e6cfed88e40d8e3088ef
Marc <teste74@hotmail.fr>
2024-08-10 02:26:38 +0200

    Update 4_GenSMBIOS.sh

--- Hackintosh/Scripts/4_GenSMBIOS.sh
+++ Hackintosh/Scripts/4_GenSMBIOS.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/bash
 
 ############################################################################################################################
-# iMacPro1,1   : NVIDIA Maxwell and Pascal or AMD Polaris and newer
+# iMacPro1,1   : NVIDIA Maxwell and Pascal or AMD Polaris and newer - AMD Navi
 # MacPro7,1    : AMD Polaris and newer
 # MacPro7,1    : Exclusive to macOS 10.15, Catalina and newer
 # MacPro6,1    : AMD GCN GPUs (supported HD and R5/R7/R9 series)
 # iMac14,2     : NVIDIA Maxwell and Pascal
 ############################################################################################################################
-
 
 ############################################################################################################################
 # Variables #
